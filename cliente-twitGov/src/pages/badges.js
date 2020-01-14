c8ad5ebd58a63cc8cf668625936133ee3404f9cc
davidlechuga <david.lechuga@lakmisystems.com.mx>
2020-01-13 20:57:01 -0600

    all ok

--- cliente-twitGov/src/pages/badges.js
+++ cliente-twitGov/src/pages/badges.js
@@ -1,49 +1,45 @@
-import React, { Component,Fragment } from 'react'
-import './styles/Badges.css'
-import {Link } from 'react-router-dom'
+import React, { Component, Fragment } from 'react';
+import './styles/Badges.css';
+import { Link } from 'react-router-dom';
 
-import BadgesList from '../components/BadgeList'
-import DB from '../data.json.js'
+import BadgesList from '../components/BadgeList';
+import DB from '../data.json';
 //import useInitialState from '../hooks/useInitialState';
 //const API = ' `https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`'
 
-
 class Badges extends Component {
+	state = {
+		datos: DB.datos
+	};
 
-    state = {
-        datos: DB.datos
-    }
-    
+	render() {
+		return (
+			<Fragment>
+				<div className="Badges">
+					<div className="Badges__heroo">
+						<div className="Badges__container">
+							<h1 className="title_one"> TwitGov Dentro Del Gobierno </h1>
+						</div>
+					</div>
+				</div>
 
-    render() {
-        return (
-            <Fragment>
+				<div className="Badge__container">
+					<div className="Badges__buttons">
+						<Link to="/" className="btn btn-primary">
+							{' '}
+							NEX PAGE >
+						</Link>
+					</div>
+				</div>
 
-                
-
-                <div className="Badges">
-                    <div className="Badges__heroo">
-                        <div className="Badges__container">
-                            <h1 className="title_one">  TwitGov  Dentro Del Gobierno </h1>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Badge__container">
-                    <div className="Badges__buttons">
-                        <Link to="/" className="btn btn-primary"> NEX PAGE ></Link>
-                    </div>
-                </div>
-
-                <div className="Badges__list">
-                    <div className="BadgesList__container">
-                        <BadgesList datos={this.state.datos} />
-                    </div>
-                 </div>
-
-            </Fragment>
-        )
-    }
+				<div className="Badges__list">
+					<div className="BadgesList__container">
+						<BadgesList datos={this.state.datos} />
+					</div>
+				</div>
+			</Fragment>
+		);
+	}
 }
 
-export default Badges
+export default Badges;
